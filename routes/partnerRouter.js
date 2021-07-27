98a5982d792dc0b5d7ffde02060b32b17a89c347
ALI <ahmedaliou94@gmail.com>
2021-07-26 20:25:38 -0400

    Express Generator REST API

---
+++ routes/partnerRouter.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const partnerRouter = express.Router();
+
+// the route is set in server.js using app.useMethod
+partnerRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end('Will send all the partner to you');
+})
+
+.post((req, res) => {
+    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+})
+
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partner');
+})
+
+.delete((req, res) => {
+    res.end('Deleting all partner');
+});
+
+
+partnerRouter.route('/:partnerId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the partners to you');
+})
+.post((req, res) => {
+    res.end(`Will add the partners: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+
+
+
+
+
+module.exports = partnerRouter;
