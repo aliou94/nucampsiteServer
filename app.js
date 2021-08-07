b5e9925f583bb1dc506217fd6a785262725718da
ALI <ahmedaliou94@gmail.com>
2021-08-07 11:06:52 -0400

    Express session part 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
@@ -31,34 +31,19 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 //auth handler
 function auth(req, res, next) {
   console.log(req.session);
-
+  
   if (!req.session.user) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
-
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-          req.session.user = 'admin';
-          return next(); // authorized
-      } else {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
   } else {
-      if (req.session.user === 'admin') {
+      if (req.session.user === 'authenticated') {
           return next();
       } else {
           const err = new Error('You are not authenticated!');
